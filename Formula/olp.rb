18662fb5a4081dadf3d29fa2d383d6a2bf8b9059
Anton Wilhelm <anton.wilhelm@here.com>
2022-05-19 14:47:10 +0200

    Switch back to real CLI

--- Formula/olp.rb
+++ Formula/olp.rb
@@ -1,15 +1,15 @@
 class Olp < Formula
-  desc "Test"
-  homepage "https://github/devtonhere/bottle-test"
-  url "https://raw.githubusercontent.com/devtonhere/bottle-test/main/bottle4.zip"
-  version "4.0.1"
-  license ""
+  desc "With the Command Line Interface you can access HERE platform services from a command line or with scripts."
+  homepage "platform.in.here.com/sdk/cli"
+  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:11.1.0/cli-11.1.0.zip\?apiKey\=84q8N9DRx6YAqyJaXUpr9fhiTnCUIBzvHLpk3QU4Oaw"
+  version "11.1.0"
+  license "Proprietary"
 
   depends_on "openjdk@8"
 
   def install
-    libexec.install "hello.jar"
-    bin.write_jar_script libexec/"hello.jar", "olp"
+    libexec.install "cli_2.12-11.1.0.jar"
+    bin.write_jar_script libexec/"cli_2.12-11.1.0.jar", "olp", java_opts: "-Dfile.encoding=UTF8"
   end
 
 end
