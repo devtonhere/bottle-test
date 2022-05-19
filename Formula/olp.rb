614674dfae3aede64e456562427e8673b7b8d136
Anton Wilhelm <anton.wilhelm@here.com>
2022-05-19 12:31:05 +0200

    Change syntax to write_jar_script

--- Formula/olp.rb
+++ Formula/olp.rb
@@ -1,15 +1,15 @@
 class Olp < Formula
   desc "Test"
   homepage "https://github/devtonhere/bottle-test"
-  url "https://raw.githubusercontent.com/devtonhere/bottle-test/main/bottle4.zip", :using => :curl
-  version "0.0.4"
+  url "https://raw.githubusercontent.com/devtonhere/bottle-test/main/bottle4.zip"
+  version "4.0.1"
   license ""
 
   depends_on "openjdk@8"
 
   def install
-    bin.install "olp"
-    bin.install "hello.jar"
+    libexec.install "hello.jar"
+    bin.write_jar_script libexec/"hello.jar", "olp"
   end
 
 end
