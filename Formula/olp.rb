5492a9761f456b875a045aaeff9fbe6485b4aa6f
Anton Wilhelm <anton.wilhelm@here.com>
2022-05-19 11:11:44 +0200

    Fix jar path

--- Formula/olp.rb
+++ Formula/olp.rb
@@ -1,8 +1,8 @@
 class Olp < Formula
   desc "Test"
   homepage "https://github/devtonhere/bottle-test"
-  url "https://raw.githubusercontent.com/devtonhere/bottle-test/main/bottle2.zip", :using => :curl
-  version "0.0.3"
+  url "https://raw.githubusercontent.com/devtonhere/bottle-test/main/bottle4.zip", :using => :curl
+  version "0.0.4"
   license ""
 
   depends_on "openjdk@8"
