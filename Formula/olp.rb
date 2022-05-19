b59db487829fdc4a1a90dc5dc37439ca980c7c92
Anton Wilhelm <anton.wilhelm@here.com>
2022-05-19 11:03:44 +0200

    Fix name

--- Formula/olp.rb
+++ Formula/olp.rb
@@ -2,13 +2,13 @@ class Olp < Formula
   desc "Test"
   homepage "https://github/devtonhere/bottle-test"
   url "https://raw.githubusercontent.com/devtonhere/bottle-test/main/bottle.zip", :using => :curl
-  version "0.0.1"
+  version "0.0.2"
   license ""
 
   depends_on "openjdk@8"
 
   def install
-    bin.install "hello-shawn"
+    bin.install "olp"
     bin.install "hello.jar"
   end
 
