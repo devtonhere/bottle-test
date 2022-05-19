bf05344b3f525a9fcbaf4ef18ef51d45df506cb5
Anton Wilhelm <anton.wilhelm@here.com>
2022-05-19 08:45:45 +0200

    Update to latest version

--- Formula/olp.rb
+++ Formula/olp.rb
@@ -1,9 +1,11 @@
 class Olp < Formula
   desc "Use the Command Line Interface (CLI) to access HERE platform services from a command line or with scripts."
   homepage "https://platform.in.here.com/sdk/cli"
-  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:10.5.125/cli-10.5.125.zip\?apiKey\=84q8N9DRx6YAqyJaXUpr9fhiTnCUIBzvHLpk3QU4Oaw", :using => :curl
-  version "2"
+  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:11.1.0/cli-11.1.0.zip\?apiKey\=84q8N9DRx6YAqyJaXUpr9fhiTnCUIBzvHLpk3QU4Oaw", :using => :curl
+  version "11.1.0"
   license ""
+
+  depends_on "openjdk@8"
 
   def install
     bin.install "olp"
